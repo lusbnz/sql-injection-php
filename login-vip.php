4b9e98150ef33e37f2d02e2b6d4447e0d334ef59
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2024-05-24 02:07:12 +0700

    brypt

--- login-vip.php
+++ login-vip.php
@@ -19,9 +19,8 @@
         if (mysqli_connect_errno()) {
             echo "<font style=\"color:#FF0000\">Could not connect:" . mysqli_connect_error() . "</font\>";
         }
-
         $user = $_POST['username'];
-        $pass = $_POST['password'];
+        $pass = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
         $stmt = $con->prepare("SELECT * FROM username WHERE user = ? AND password = ?");
         $stmt->bind_param("ss", $user, $pass);
@@ -33,30 +32,26 @@
         if ($result->num_rows > 0) {
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
-                    echo "<p>User: " . htmlspecialchars($row['user']) . "</p>";
-                    echo "<p>Password: " . htmlspecialchars($row['password']) . "</p>";
-                    echo "<p>First Name: " . htmlspecialchars($row['fname']) . "</p>";
-                    echo "<p>Last Name: " . htmlspecialchars($row['lname']) . "</p>";
-                    echo "<p>Gender: " . htmlspecialchars($row['gender']) . "</p>";
-                    echo "<p>Date of Birth: " . htmlspecialchars($row['dob']) . "</p>";
-                    echo "<p>Country: " . htmlspecialchars($row['country']) . "</p>";
-                    echo "<p>Score: " . htmlspecialchars($row['score']) . "</p>";
-                    echo "<p>Email: " . htmlspecialchars($row['email']) . "</p>";
+                    echo "<p>" . "User : " . $row[0] . "</p>";
+                    echo "<p>" . "Password : " . $row[1] . "</p>";
+                    echo "<p>" . "First Name : " . $row[2] . "</p>";
+                    echo  "<p>" . "Last Name : " . $row[3] . "</p>";
+                    echo "<p>" . "Gender : " . $row[4] . "</p>";
+                    echo "<p>" . " Date of Birth :" . $row[5] . "</p>";
+                    echo "<p>" . "Country : " . $row[6] . "</p>";
+                    echo "<p>" . "Score : " . $row[7] . "</p>";
+                    echo "<p>" . "Email : " . $row[8] . "</p>";
                     echo "--------------------------------------------<br/>";
                 }
             } else {
                 echo "Tên đăng nhập hoặc mật khẩu không hợp lệ";
             }
-
             $stmt->close();
-
         } else {
             echo "Vui lòng đăng nhập lại";
         }
-
         $con->close();
         ?>
-
     </div>
 </body>
 </html>
